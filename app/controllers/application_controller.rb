c199118751f88105c54b12f40b5a20ca8e00636c
dnabrock <dnabrock@gmail.com>
2012-10-08 16:50:12 +0700

    update version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 # module I18n; def self.t(id); '~'; end; end
 
 class ApplicationController < ActionController::Base
-  APPLICATION_VERSION = '1.1.19'
+  APPLICATION_VERSION = '1.1.21'
 
   protect_from_forgery
 
