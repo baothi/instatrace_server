93884b8a21e10c5244deb1214196268023243e5d
dnabrock <dnabrock@gmail.com>
2012-10-15 13:15:22 +0700

    118 -emails from phone app not being sent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 # module I18n; def self.t(id); '~'; end; end
 
 class ApplicationController < ActionController::Base
-  APPLICATION_VERSION = '1.1.22'
+  APPLICATION_VERSION = '1.1.23'
 
   protect_from_forgery
 
