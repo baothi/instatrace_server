bf32fe5115a5269d2e83baf740e57502e1be6825
dnabrock <dnabrock@gmail.com>
2012-10-18 18:02:21 +0700

    #114 - Convert to Web Service for interface with World-Trak

--- app/admin/shipments.rb
+++ app/admin/shipments.rb
@@ -8,14 +8,14 @@ ActiveAdmin.register Shipment do
     column(:hawb) {|s| link_to s.hawb, admin_shipment_path(s)}
     column :service_level
     column :mawb
-    column :pieces
-    column("Weight") {|s| "#{number_with_delimiter(s.weight.to_i)} Lb." }
-    column :origin
-    column :destination
+    column :pieces_total
+    column("Weight") { |s| "#{number_with_delimiter(s.weight.to_i)} Lb." }
+    column(:origin) { |s| raw "#{s.origin}" }       
+    column(:destination) { |s| raw "#{s.destination}" }  
     column :shipper
     column :consignee
-    column :ship
-    column :delivery
+    column :ship_date
+    column :delivery_date
     column "Registered", :created_at
   end
 
@@ -26,14 +26,14 @@ ActiveAdmin.register Shipment do
       row :service_level
       row :hawb
       row :mawb
-      row :pieces
+      row :pieces_total
       row (:weight) { "#{number_with_delimiter(s.weight.to_i)} Lb." }
-      row :origin
-      row :destination
+      row (:origin) { raw "#{s.origin}"}  
+      row (:destination) {raw "#{s.destination}"}  
       row :shipper
       row :consignee
-      row :ship
-      row :delivery
+      row :ship_date
+      row :delivery_date
     end
     active_admin_comments
   end
@@ -45,16 +45,164 @@ ActiveAdmin.register Shipment do
       f.input :service_level
       f.input :hawb
       f.input :mawb
-      f.input :pieces
+      f.input :pieces_total
       f.input :weight
-      f.input :origin
-      f.input :destination
+      #f.input :origin
+      f.input :origin_address1
+      f.input :origin_address2
+      f.input :origin_city
+      f.input :origin_state
+      f.input :origin_zip_postal_code
+      #f.input :destination
+      f.input :dest_address1
+      f.input :dest_address2
+      f.input :dest_city
+      f.input :dest_state
+      f.input :dest_zip_postal_code
       f.input :shipper
       f.input :consignee
-      f.input :ship
-      f.input :delivery
+      f.input :ship_date
+      f.input :delivery_date
     end
     f.buttons
   end
+
+  collection_action :update, :method => :put do
+      @shipment = Shipment.find(params[:id])
+                
+      #Concat origin data
+      # if params[:shipment][:origin_address1] && !params[:shipment][:origin_address1].blank?
+      #     @shipment.origin =  params[:shipment][:origin_address1]           
+      # end
+      
+      # if params[:shipment][:origin_address2] && !params[:shipment][:origin_address2].blank?        
+      #   @shipment.origin <<  "<br/>" + params[:shipment][:origin_address2] + "<br/>"
+      #   @shipment.origin_address2 =  params[:shipment][:origin_address2] 
+      # end       
+
+      # @shipment.origin << params[:shipment][:origin_city]
+      # if params[:shipment][:origin_state] && !params[:shipment][:origin_state].blank?
+      #   @shipment.origin << ", "
+      # end  
+      #  @shipment.origin << params[:shipment][:origin_state]   + " " + params[:shipment][:origin_zip_postal_code] 
+      
+      # #Concat destination data     
+      # if params[:shipment][:dest_address1] && !params[:shipment][:dest_address1].blank?
+      #   @shipment.destination =  params[:shipment][:dest_address1] 
+      # end
+      
+      # if params[:shipment][:dest_address2] && !params[:shipment][:dest_address2].blank?
+      #   @shipment.destination <<  "<br/>" + params[:shipment][:dest_address2] + "<br/>"
+      # end               
+      # @shipment.destination <<  params[:shipment][:dest_city] 
+      # if params[:shipment][:dest_state] && !params[:shipment][:dest_state].blank?
+      #    @shipment.destination << ", "
+      # end
+
+      # @shipment.destination << params[:shipment][:dest_state]   + " " + params[:shipment][:dest_zip_postal_code] 
+
+      # @shipment.origin_address1 =  params[:shipment][:origin_address1]
+      # @shipment.origin_address2 =  params[:shipment][:origin_address2]
+      # @shipment.origin_state = params[:shipment][:origin_state]
+      # @shipment.origin_city = params[:shipment][:origin_city]
+      # @shipment.origin_zip_postal_code = params[:shipment][:origin_zip_postal_code]
+
+      # @shipment.dest_address1 = params[:shipment][:dest_address1]
+      # @shipment.dest_address2 = params[:shipment][:dest_address2]
+      # @shipment.dest_state = params[:shipment][:dest_state]
+      # @shipment.dest_city = params[:shipment][:dest_city]
+      # @shipment.dest_zip_postal_code = params[:shipment][:dest_zip_postal_code]
+
+      # @shipment.shipment_id = params[:shipment][:shipment_id]
+      # @shipment.service_level = params[:shipment][:service_level]
+      # @shipment.pieces_total = params[:shipment][:pieces_total]
+      # @shipment.weight = params[:shipment][:weight]
+      # @shipment.shipper = params[:shipment][:shipper]
+      # @shipment.consignee = params[:shipment][:consignee]
+
+      # ship_date = params[:shipment]["ship_date(1i)"]<<"-"<< params[:shipment]["ship_date(2i)"]<<"-"<<params[:shipment]["ship_date(3i)"]<<" "<<params[:shipment]["ship_date(4i)"]<<":"<<params[:shipment]["ship_date(5i)"]<<":00"
+      # @shipment.ship_date = Date.strptime(ship_date, '%Y-%m-%d %H:%M:%S') rescue nil
+      # delivery_date = params[:shipment]["delivery_date(1i)"]<<"-"<< params[:shipment]["delivery_date(2i)"]<<"-"<<params[:shipment]["delivery_date(3i)"]<<" "<<params[:shipment]["delivery_date(4i)"]<<":"<<params[:shipment]["delivery_date(5i)"]<<":00"
+      # @shipment.delivery_date = Date.strptime(delivery_date, '%Y-%m-%d %H:%M:%S') rescue nil 
+     
+      # @shipment.save
+      set_shipment_data params,@shipment
+    
+      redirect_to admin_shipment_path(@shipment.id) 
+  end
+
+  collection_action :create, :method => :post do
+      @shipment = Shipment.new params[:shipment]
+     
+      set_shipment_data params,@shipment
+       
+      redirect_to admin_shipment_path(@shipment.id) 
+  end
+
+  
+
+  controller do
+    def set_shipment_data (params, shipment)
+      #Concat origin data
+      if params[:shipment][:origin_address1] && !params[:shipment][:origin_address1].blank?
+          shipment.origin =  params[:shipment][:origin_address1]           
+      end
+      
+      if params[:shipment][:origin_address2] && !params[:shipment][:origin_address2].blank?        
+        shipment.origin +=  "<br/>" + params[:shipment][:origin_address2] + "<br/>"        
+      end       
+
+      shipment.origin += params[:shipment][:origin_city]
+      if params[:shipment][:origin_state] && !params[:shipment][:origin_state].blank?
+        shipment.origin += ", "
+      end  
+       shipment.origin += params[:shipment][:origin_state]   + " " + params[:shipment][:origin_zip_postal_code] 
+      
+      #Concat destination data     
+      if params[:shipment][:dest_address1] && !params[:shipment][:dest_address1].blank?
+        shipment.destination =  params[:shipment][:dest_address1] 
+      end
+      
+      if params[:shipment][:dest_address2] && !params[:shipment][:dest_address2].blank?
+        shipment.destination +=  "<br/>" + params[:shipment][:dest_address2] + "<br/>"
+      end               
+      shipment.destination +=  params[:shipment][:dest_city] 
+      if params[:shipment][:dest_state] && !params[:shipment][:dest_state].blank?
+         shipment.destination += ", "
+      end
+
+      shipment.destination += params[:shipment][:dest_state]   + " " + params[:shipment][:dest_zip_postal_code] 
+
+      shipment.origin_address1 =  params[:shipment][:origin_address1]
+      shipment.origin_address2 =  params[:shipment][:origin_address2]
+      shipment.origin_state = params[:shipment][:origin_state]
+      shipment.origin_city = params[:shipment][:origin_city]
+      shipment.origin_zip_postal_code = params[:shipment][:origin_zip_postal_code]
+
+      shipment.dest_address1 = params[:shipment][:dest_address1]
+      shipment.dest_address2 = params[:shipment][:dest_address2]
+      shipment.dest_state = params[:shipment][:dest_state]
+      shipment.dest_city = params[:shipment][:dest_city]
+      shipment.dest_zip_postal_code = params[:shipment][:dest_zip_postal_code]
+
+      shipment.shipment_id = params[:shipment][:shipment_id]
+      shipment.service_level = params[:shipment][:service_level]
+      shipment.pieces_total = params[:shipment][:pieces_total]
+      shipment.weight = params[:shipment][:weight]
+      shipment.shipper = params[:shipment][:shipper]
+      shipment.consignee = params[:shipment][:consignee]
+
+      ship_date = params[:shipment]["ship_date(1i)"]<<"-"<< params[:shipment]["ship_date(2i)"]<<"-"<<params[:shipment]["ship_date(3i)"]<<" "<<params[:shipment]["ship_date(4i)"]<<":"<<params[:shipment]["ship_date(5i)"]<<":00"
+      shipment.ship_date = Date.strptime(ship_date, '%Y-%m-%d %H:%M:%S') rescue nil
+      delivery_date = params[:shipment]["delivery_date(1i)"]<<"-"<< params[:shipment]["delivery_date(2i)"]<<"-"<<params[:shipment]["delivery_date(3i)"]<<" "<<params[:shipment]["delivery_date(4i)"]<<":"<<params[:shipment]["delivery_date(5i)"]<<":00"
+      shipment.delivery_date = Date.strptime(delivery_date, '%Y-%m-%d %H:%M:%S') rescue nil 
+     
+      shipment.save
+    end
+  end
+  
+
+
+
 end
 
