c8bc73e70e6af1249f14d58eec4c50d9d38117f8
dnabrock <dnabrock@gmail.com>
2012-10-18 18:02:47 +0700

    #114 - Convert to Web Service for interface with World-Trak

--- app/controllers/api/shipments_controller.rb
+++ app/controllers/api/shipments_controller.rb
@@ -1,12 +1,58 @@
 class Api::ShipmentsController < Api::ApiController
-  before_filter :get_shipment, :except => :mass_update 
+  before_filter :get_shipment, :except => [:mass_update ,:post_shipment]
+  include ActionView::Helpers
   
   def show
     current_user.current_milestone.update_attributes :shipment_id => @shipment.id, :damaged => @shipment.damaged?#, :damage_desc => @shipment.damage_desc
     render :json => @shipment
   end
-  
-  def mass_update
+  def post_shipment               
+    if params[:shipment]        
+        #Update params data because of update_attributes rails rules
+        if params[:shipment][:pieces].present?
+           params[:shipment][:pieces_attributes] = params[:shipment][:pieces]
+           params[:shipment].delete :pieces           
+        end 
+        @shipment = Shipment.find_by_hawb(params[:shipment][:hawb])
+        
+        if @shipment            
+            @shipment.update_attributes(params[:shipment])
+        else
+           @shipment = Shipment.new params[:shipment]           
+        end
+        
+        @shipment.origin =  params[:shipment][:origin_address1] 
+        if params[:shipment][:origin_address2] 
+           @shipment.origin << "<br/>" + params[:shipment][:origin_address2] + "<br/>"
+        end       
+        @shipment.origin << params[:shipment][:origin_city] + ", " + params[:shipment][:origin_state]   + " " + params[:shipment][:origin_zip_postal_code] 
+
+        @shipment.destination = params[:shipment][:dest_address1] 
+        if params[:shipment][:dest_address2]
+           @shipment.destination << "<br/>" + params[:shipment][:dest_address2] + "<br/>" 
+        end 
+        @shipment.destination << params[:shipment][:dest_city] << ", " << params[:shipment][:dest_state]   <<  " "  <<   params[:shipment][:dest_zip_postal_code] 
+
+        @shipment.ship_date = Date.strptime(params[:shipment][:ship_date], '%Y-%m-%d %H:%M:%S') rescue nil
+        @shipment.delivery_date = Date.strptime(params[:shipment][:delivery_date], '%Y-%m-%d %H:%M:%S') rescue nil          
+
+        if @shipment.save! 
+           piece = Piece.find_by_sql ["SELECT SUM(weight) as weight, SUM(height) as height, SUM(length) as length, count(id) as pieces_total FROM pieces WHERE shipment_id = ? ",@shipment.id]
+           if piece     
+              piece = piece.first         
+              #Update data of shipment table             
+              @shipment.update_attributes(:weight => piece.weight, :height => piece.height, :length => piece.length, :pieces_total => piece.pieces_total )              
+              @shipment.save!            
+           end                           
+
+        end
+        render :json => {:status => 200, :message => t('messages.notice.milestone_created_ok')}
+    else        
+      raise Exception, t('errors.messages.not_found') 
+    end
+  end
+
+  def mass_update    
      begin
       if params[:data]
         data = ActiveSupport::JSON.decode(params[:data])
@@ -46,6 +92,8 @@ class Api::ShipmentsController < Api::ApiController
         render :status => 500, :json => {:status => false, :message => e.message};
     end  
   end
+
+ 
   
 protected
   
