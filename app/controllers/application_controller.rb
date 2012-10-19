7758300a2fee9e1bb4a0f9bb63f39bbf4fb6d5ff
dnabrock <dnabrock@gmail.com>
2012-10-19 17:10:42 +0700

    #127-last location is pulling from old last location from another user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 # module I18n; def self.t(id); '~'; end; end
 
 class ApplicationController < ActionController::Base
-  APPLICATION_VERSION = '1.1.24'
+  APPLICATION_VERSION = '1.1.25'
 
   protect_from_forgery
 
