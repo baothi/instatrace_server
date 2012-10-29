f0e99ae7bd3407e712813ea84bf2dba4937ca6b9
dnabrock <dnabrock@gmail.com>
2012-10-29 14:19:39 +0700

    #122 - real time tracking functional spec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 # module I18n; def self.t(id); '~'; end; end
 
 class ApplicationController < ActionController::Base
-  APPLICATION_VERSION = '1.1.26'
+  APPLICATION_VERSION = '1.1.27'
 
   protect_from_forgery
 
