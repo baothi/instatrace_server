8a81960b2672ce06b8a6c812cc0f23304be21885
TBA <udev@ubuntu.(none)>
2012-11-02 02:16:12 -0700

    #50,#104 show city and state in location in tracking detail

--- app/workers/milestone_geocoder.rb
+++ app/workers/milestone_geocoder.rb
@@ -3,6 +3,8 @@ class MilestoneGeocoder
 
   def self.perform(milstone_id)
     milestone = Milestone.find(milstone_id)
+    geo = Geocoder.search("#{milestone.latitude},#{milestone.longitude}")[0]  	
+  	milestone.update_attribute :address, geo.city + ", " + geo.state
     #milestone.update_attribute :address, milestone.reverse_geocode_wrap
   end
 end
