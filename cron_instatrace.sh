012fd3bbabef056a78ebe8a21abaa10a965f43f7
ngocnguyen <nvngoc@tma.com.vn>
2012-11-28 11:44:48 +0700

    #130 - Retrieve EDI feed from Deferred Air Carriers

--- cron_instatrace.sh
+++ cron_instatrace.sh
@@ -3,5 +3,6 @@ echo "=================Start Importing=============="
 
 source ~/.rvm/scripts/rvm
 RAILS_ENV=production bundle exec rake import:shipments
+RAILS_ENV=production bundle exec rake import:milestones
 
 echo Importing have done at `date`
