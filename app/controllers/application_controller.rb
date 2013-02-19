4b759ebbb3efdcb11fc433600d36a7b71d03a04d
ngocnguyen <nvngoc@tma.com.vn>
2013-02-19 09:58:37 +0700

    Version 1.1.54 - Fix 165: requested delivery date no displaying on milestone screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 # module I18n; def self.t(id); '~'; end; end
 
 class ApplicationController < ActionController::Base
-  APPLICATION_VERSION = '1.1.53'
+  APPLICATION_VERSION = '1.1.54'
 
   protect_from_forgery
 
