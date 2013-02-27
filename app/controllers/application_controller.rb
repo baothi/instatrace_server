455f76ccbdf56e6a53b0da0ac1ec23479b41d8af
ngocnguyen <nvngoc@tma.com.vn>
2013-02-27 10:50:24 +0700

    Version 1.1.57 - Fix 169:  	change data field for Updated by from login to Last, first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 # module I18n; def self.t(id); '~'; end; end
 
 class ApplicationController < ActionController::Base
-  APPLICATION_VERSION = '1.1.56'
+  APPLICATION_VERSION = '1.1.57'
 
   protect_from_forgery
 
